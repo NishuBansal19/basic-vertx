9bf55180b12e702109eaf417b418e93ed18f5064
nisbansa <nisbansa@adobe.com>
2022-03-13 13:50:01 +0530

    Add Mongo and event bus configuration

--- src/main/java/com/vertx/Application.java
+++ src/main/java/com/vertx/Application.java
@@ -1,25 +1,53 @@
 package com.vertx;
 
 import com.vertx.dto.MyItem;
+import com.vertx.entity.Product;
+import com.vertx.resources.ProductResource;
+import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.json.Json;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.FindOptions;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.StaticHandler;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application extends AbstractVerticle {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    private static MongoClient mongoClient = null;
+
     public static void main(String[] args) {
-        Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle(new Application());
+
+        VertxOptions vertxOptions = new VertxOptions();
+        vertxOptions.setClustered(true);
+        Vertx.clusteredVertx(vertxOptions, results -> {
+            if (results.succeeded()) {
+                Vertx vertx = results.result();
+                ConfigRetriever configRetriever = ConfigRetriever.create(vertx);
+                configRetriever.getConfig(config -> {
+                    if(config.succeeded()) {
+                        JsonObject configJson = config.result();
+                        LOGGER.info(configJson.encodePrettily());
+                        DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(configJson);
+                        vertx.deployVerticle(new Application(), deploymentOptions);
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -27,7 +55,57 @@ public class Application extends AbstractVerticle {
         LOGGER.info("Vertx Started");
         processJson();
         // createBasicWebServer(vertx);
-        createWebServer(vertx);
+        // createWebServer(vertx);
+        createEventBus(vertx);
+    }
+
+    public void createEventBus(Vertx vertx) {
+        Router router = Router.router(vertx);
+
+        router.get("/mongofind").handler(this::getAllProductsFromMongoDB);
+
+        JsonObject dbConfig = new JsonObject();
+
+        dbConfig.put("connection_string", "mongodb://localhost:27017/Mongotest");
+        // Config for authentication
+		dbConfig.put("username", "testUser");
+		dbConfig.put("password", "test");
+		dbConfig.put("authSource", "Mongotest");
+        dbConfig.put("useObjectId", true);
+
+        mongoClient = MongoClient.createShared(vertx, dbConfig);
+
+
+        // Consumer for listening events
+//		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+//
+//		vertx.eventBus().consumer("com.vertx", message -> {
+//
+//			System.out.println("Recevied message: " + message.body());
+//
+//			message.reply(new JsonObject().put("responseCode", "OK").put("message", "This is your response to your event"));
+//
+//		});
+
+        vertx.setTimer(5000, handler ->{
+            sendTestEvent();
+        });
+    }
+
+    private void sendTestEvent() {
+
+        JsonObject testInfo = new JsonObject();
+
+        testInfo.put("info", "Hi");
+
+        System.out.println("Sending message=" + testInfo.toString());
+
+        vertx.eventBus().send("com.vertx", testInfo.toString(), reply -> {
+            if (reply.succeeded()) {
+                JsonObject replyResults = (JsonObject) reply.result().body();
+                System.out.println("Got Reply message=" + replyResults.toString());
+            }
+        });
     }
 
     public void createBasicWebServer(Vertx vertx) {
@@ -38,6 +116,12 @@ public class Application extends AbstractVerticle {
 
     public void createWebServer(Vertx vertx){
         Router router = Router.router(vertx);
+
+        // Create ProductResource object
+        ProductResource productResources = new ProductResource();
+
+        // Map subrouter for Products
+        router.mountSubRouter("/api/", productResources.getAPISubRouter(vertx));
 
         router.get("/yo.html").handler(routingContext -> {
 
@@ -65,7 +149,14 @@ public class Application extends AbstractVerticle {
 
         // Default if no routes are matched
         router.route().handler(StaticHandler.create().setCachingEnabled(false));
-        vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+        vertx.createHttpServer().requestHandler(router::accept).listen(config().getInteger("http.port"), asyncResult -> {
+            if (asyncResult.succeeded()) {
+                LOGGER.info("HTTP server running on port " + config().getInteger("http.port"));
+            }
+            else {
+                LOGGER.error("Could not start a HTTP server", asyncResult.cause());
+            }
+        });
     }
 
     public String replaceAllTokens(String input, String token, String newValue) {
@@ -92,6 +183,43 @@ public class Application extends AbstractVerticle {
 
         JsonObject jsonObject2  = JsonObject.mapFrom(myitem);
         LOGGER.info(jsonObject2.toString());
+    }
+
+
+    // Get all products as array of products
+    private void getAllProductsFromMongoDB(RoutingContext routingContext) {
+        FindOptions findOptions = new FindOptions();
+        //findOptions.setLimit(1);
+        mongoClient.findWithOptions("products", new JsonObject(), findOptions, results -> {
+            try {
+                List<JsonObject> objects = results.result();
+                if (objects != null && objects.size() != 0) {
+                    System.out.println("Got some data len=" + objects.size());
+                    JsonObject jsonResponse = new JsonObject();
+                    jsonResponse.put("products", objects);
+                    routingContext.response()
+                            .putHeader("content-type", "application/json; charset=utf-8")
+                            .setStatusCode(200)
+                            .end(Json.encodePrettily(jsonResponse));
+                } else {
+                    JsonObject jsonResponse = new JsonObject();
+                    jsonResponse.put("error", "No items found");
+                    routingContext.response()
+                            .putHeader("content-type", "application/json; charset=utf-8")
+                            .setStatusCode(400)
+                            .end(Json.encodePrettily(jsonResponse));
+                }
+
+            } catch (Exception e) {
+                LOGGER.info("getAllProducts Failed exception e=", e.getLocalizedMessage());
+                JsonObject jsonResponse = new JsonObject();
+                jsonResponse.put("error", "Exception and No items found");
+                routingContext.response()
+                        .putHeader("content-type", "application/json; charset=utf-8")
+                        .setStatusCode(500)
+                        .end(Json.encodePrettily(jsonResponse));
+            }
+        });
     }
 
     @Override
---
+++ src/main/java/com/vertx/entity/Product.java
@@ -0,0 +1,41 @@
+package com.vertx.entity;
+
+public class Product {
+
+    private String number;
+    private String id;
+    private String description;
+
+    public Product(String id, String number, String description) {
+        this.id = id;
+        this.number = number;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+}
